e56a9953a5d38ec8b88c46716815e36092025f55
Arjun Sawhney <arjun@crowdai.com>
2020-11-03 00:52:48 -0800

    added sim code and code to make obs space multidiscrete

--- myenvs/envs/rps_env.py
+++ myenvs/envs/rps_env.py
@@ -10,9 +10,12 @@ class RPSEnv(gym.Env):
     def __init__(self, opp_policy, max_games=20):
         super(RPSEnv, self).__init__()
         self.games_count = 0
+        # self.policy_count = 0
+        # self.opponent_policies = opp_policies
+        # self.opponent_policy = self.opponent_policies[self.policy_count % len(self.opponent_policies)]
         self.opponent_policy = opp_policy
         self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
-        self.observation_space = spaces.Discrete(N_DISCRETE_ACTIONS)
+        self.observation_space = spaces.MultiDiscrete([N_DISCRETE_ACTIONS, N_DISCRETE_ACTIONS])
         self.max_games = max_games
 
     def get_reward(self, action_one, action_two):
@@ -20,12 +23,16 @@ class RPSEnv(gym.Env):
         we_lose = (action_one == (action_two - 1) % 3)
         return int(we_win) - int(we_lose)
 
-    def step(self, action):
+    def step(self, action, opponent_policy = None):
         my_action = action
-        opponent_action = self.opponent_policy.get_next_move()
-        self.opponent_policy.update_opponent_history(my_action)
+        if opponent_policy is None:
+            opponent_action = self.opponent_policy.get_next_move()
+            self.opponent_policy.update_opponent_history(my_action)
+        else:
+            opponent_action = opponent_policy.get_next_move()
+            opponent_policy.update_opponent_history(my_action)
 
-        obs = opponent_action
+        obs = np.array([opponent_action, my_action])
         reward = self.get_reward(my_action, opponent_action)
         self.games_count += 1
         done = (self.games_count == self.max_games)
@@ -35,11 +42,25 @@ class RPSEnv(gym.Env):
     def reset(self):
         # Reset the state of the environment to an initial state
         self.games_count = 0
+        # self.policy_count += 1
+        # self.opponent_policy = self.opponent_policies[self.policy_count % len(self.opponent_policies)]
         self.opponent_policy.reset_policy()
         self.render()
-        return 0
+        return np.array([0, 0])
 
     def render(self, mode='human', close=False):
         # Render the environment to the screen
         # return self.opponent_policy.opponent_history, self.opponent_policy.history
         pass
+
+    def run_sim(self, curr_policy, num_games, model):
+        self.games_count = 0
+        curr_policy.reset_policy()
+        rewards_list = []
+        obs = np.array([0, 0])
+        for i in range(num_games):
+            action, _states = model.predict(obs)
+            obs, rewards, dones, info = self.step(action, opponent_policy = curr_policy)
+            rewards_list.append(rewards)
+        return rewards_list
+
--- run_mlp_policy.py
+++ run_mlp_policy.py
@@ -5,6 +5,11 @@ import torch as th
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
 from rps_policy import policies
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+plt.style.use('ggplot')
+plt.rcParams.update({'font.size': 7})
 
 # FOR REFERENCE
 # policies[0] = fixed_rock_policy,
@@ -15,21 +20,46 @@ from rps_policy import policies
 # policies[5] = aggressive_policy,
 # policies[6] = passive_policy,
 
-opp_policy = policies[5]
+opp_policy = policies[6]
 env = gym.make('rps-v0', opp_policy=opp_policy)
 test_env = gym.make('rps-v0', opp_policy=opp_policy)
+
+# # env = gym.make('rps-v0', opp_policies=policies)
+# # test_env = gym.make('rps-v0', opp_policies=policies)
 eval_callback = EvalCallback(test_env, eval_freq=10, deterministic=True, render=False)
 
 
-policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=[8, 8])
+policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=[8,8])
 n_steps, batch_size, n_epochs =  10, 10, 10
 
 model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, n_steps=n_steps, batch_size=batch_size, n_epochs=n_epochs, verbose=0)
-model.learn(total_timesteps=200, callback=eval_callback)
+model.learn(total_timesteps=1000, callback=eval_callback)
+rewards_fixed_rock = env.run_sim(policies[0], 50, model)
+rewards_fixed_paper = env.run_sim(policies[1], 50, model)
+rewards_fixed_scissors = env.run_sim(policies[2], 50, model)
+rewards_copycat = env.run_sim(policies[3], 50, model)
+rewards_random = env.run_sim(policies[4], 50, model)
+rewards_aggressive = env.run_sim(policies[5], 50, model)
+rewards_passive = env.run_sim(policies[6], 50, model)
+avg_rewards = [np.mean(rewards_fixed_rock), np.mean(rewards_fixed_paper), np.mean(rewards_fixed_scissors), np.mean(rewards_copycat), np.mean(rewards_random), np.mean(rewards_aggressive), np.mean(rewards_passive)]
+x = [policy.name for policy in policies]
+x_pos = [i for i, _ in enumerate(x)]
+plt.bar(x_pos, avg_rewards, color='red')
+plt.xlabel("Opponent Policy")
+plt.ylabel("Average Simulated Reward")
+plt.title("Individual Agents Trained (Expanded State Space): Average Simulated Reward vs Opponent Policy")
+plt.xticks(x_pos, x)
+plt.show()
 
-# Save the agent
-model.save("ppo-rps")
 
-del model
-# the policy_kwargs are automatically loaded
-model = PPO.load("ppo-rps")
+
+# model = PPO2("MlpLstmPolicy", env, nminibatches=1, policy_kwargs=policy_kwargs, n_steps=n_steps, batch_size=batch_size, n_epochs=n_epochs, verbose=0)
+# model.learn(total_timesteps= 2000, callback = eval_callback)
+
+
+# # Save the agent
+# model.save("ppo-rps")
+
+# del model
+# # the policy_kwargs are automatically loaded
+# model = PPO.load("ppo-rps")
